de885eb481039bd18a08c906dc622cf4df3c0f93
aayush yadav <aayushyadavg@gmail.com>
2019-11-21 08:10:40 +0545

    Initial commit

---
+++ app/src/main/java/com/example/topic4/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.topic4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String countries[]={
+            "Nepal","Kathmandu",
+            "India", "New Dehli",
+            "China", "BEijing",
+            "UK","london",
+            "US","wasington",
+            "Canada","Ottawa"
+    };
+    private Map<String,String> dictionary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView Countries=findViewById(R.id.lstcountries);
+
+        dictionary= new HashMap<>();
+        for(int i=0;i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter= new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet())
+        );
+
+        Countries.setAdapter(countryAdapter);
+    }
+}
