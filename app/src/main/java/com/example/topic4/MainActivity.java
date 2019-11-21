afa4bee3b3d25878c77fb9d1f5a3eed1744d7c21
aayush yadav <aayushyadavg@gmail.com>
2019-11-21 08:25:47 +0545

    sacch

--- app/src/main/java/com/example/topic4/MainActivity.java
+++ app/src/main/java/com/example/topic4/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,5 +43,16 @@ public class MainActivity extends AppCompatActivity {
         );
 
         Countries.setAdapter(countryAdapter);
+        Countries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+          String Country=parent.getItemAtPosition(position).toString();
+          String Capital=dictionary.get(Country);
+                Toast.makeText(getApplicationContext(), Capital.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
+
 }
